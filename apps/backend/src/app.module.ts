2d24cb2107237a98ddce7ea02410dc05e5be40ca
Mark Baker <mark@vpost.net>
2025-12-07 23:51:33 -0500

    style: standardize quotation marks to double quotes in app files

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TechnitiumModule } from './technitium/technitium.module';
-import { BlockListCatalogModule } from './blocklist-catalog/blocklist-catalog.module';
+import { Module } from "@nestjs/common";
+import { CacheModule } from "@nestjs/cache-manager";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TechnitiumModule } from "./technitium/technitium.module";
+import { BlockListCatalogModule } from "./blocklist-catalog/blocklist-catalog.module";
 
 @Module({
   imports: [
